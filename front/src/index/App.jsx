56f46076d7ea8c2b61bd4b6468b8770838400ecd
yangzqv <yangzqv@126.com>
2020-04-22 15:59:37 +0800

    perfect

--- front/src/index/App.jsx
+++ front/src/index/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useMemo } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import './App.css'
@@ -22,24 +22,28 @@ function App(props) {
     window.history.back()
   }, [])
 
-  const cbs = bindActionCreators({
-    exchangeFromTo,
-    showCitySelector
-  }, dispatch)
+  const cbs = useMemo(() => {
+    return bindActionCreators({
+      exchangeFromTo,
+      showCitySelector
+    }, dispatch)
+  }, [])
 
   return (
     <div>
       <div className="header-wrapper">
         <Header title="火车票" onBack={onBack} />
       </div>
-      <Journey
-        from={from}
-        to={to}
-        {...cbs}
-      />
-      <DepartDate />
-      <HighSpeed />
-      <Submit />
+      <form className="form">
+        <Journey
+          from={from}
+          to={to}
+          {...cbs}
+        />
+        <DepartDate />
+        <HighSpeed />
+        <Submit />
+      </form>
     </div>
   )
 }
